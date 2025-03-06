fafdbdd34127dc4c02a4efc9e936284f2c11d223
3illBaby <thrillanon@outlook.com>
2025-03-06 20:54:00 +0100

    feat(hero.tsx): impl hero component

---
+++ components/hero.tsx
@@ -0,0 +1,39 @@
+import { GlowEffectButton } from "./glow-button";
+import { TextEffect } from "./motion-primitives/text-effect";
+
+const Hero = () => {
+  return (
+    <section className="section_wrapper bg-grid-white/[0.07]">
+      <div className="flex_end">
+        <GlowEffectButton />
+      </div>
+
+      <div className="flex_center relative mt-28 w-full gap-5">
+        <TextEffect
+          per="char"
+          preset="fade-in-blur"
+          speedReveal={0.5}
+          className="font-bebas sm:text-wider relative text-7xl font-extrabold tracking-wide text-neutral-300 font-stretch-90% sm:text-9xl lg:text-[200px]"
+        >
+          Mchango
+        </TextEffect>
+
+        <TextEffect
+          per="word"
+          preset="fade-in-blur"
+          speedReveal={0.7}
+          delay={0.5}
+          className="font-grotesk text-center text-sm font-light text-neutral-300 capitalize lg:text-lg"
+        >
+          Eliminate trust from group savings and contributions
+        </TextEffect>
+
+        <div className="flex_center mt-5 w-full">
+          <GlowEffectButton />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
