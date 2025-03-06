96f01a0af61e898807b1819449f314a3cd7c1eb1
3illBaby <thrillanon@outlook.com>
2025-03-06 20:51:12 +0100

    chore(page.tsx): added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import Hero from "@/components/hero";
+
 const Landing = () => {
   return (
-    <section className="section_wrapper">
-      <h1 className="font-grotesk motion-preset-expand motion-duration-700 text-5xl font-extrabold text-neutral-50 font-stretch-95%">
-        Welcome to Mchango
-      </h1>
-    </section>
+    <>
+      <Hero />
+    </>
   );
 };
 
