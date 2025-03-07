f3e7cf5da1a7f922d9541fd3c26b155d203f066e
3illBaby <thrillanon@outlook.com>
2025-03-07 16:13:32 +0100

    refactor(how-it-works.tsx): redited section

--- components/how-it-works.tsx
+++ components/how-it-works.tsx
@@ -30,14 +30,11 @@ const HowItWorks = () => {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, ease: "easeIn", type: "tween" }}
-            className="flex max-w-prose flex-row items-center gap-1 sm:gap-3"
+            className="flex max-w-prose"
           >
-            <motion.h3 className="work_header">
+            <motion.h3 className="work_header !bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-700 bg-clip-text text-transparent">
               Step 1: Create / Join a group
             </motion.h3>
-            <div className="flex h-[50px] w-[20px] rounded-sm bg-yellow-500">
-              {" "}
-            </div>
           </motion.div>
 
           <p className="work_description">
@@ -54,12 +51,9 @@ const HowItWorks = () => {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, ease: "easeIn", type: "tween" }}
-            className="flex max-w-prose flex-row items-center gap-1 sm:gap-3"
+            className="flex max-w-prose"
           >
-            <div className="flex h-[50px] w-[20px] rounded-sm bg-green-500">
-              {" "}
-            </div>
-            <motion.h3 className="work_header">
+            <motion.h3 className="work_header bg-gradient-to-r from-green-300 via-green-500 to-green-700 bg-clip-text text-transparent">
               Step 2: Stake ERC20 Collateral
             </motion.h3>
           </motion.div>
@@ -73,22 +67,20 @@ const HowItWorks = () => {
         </div>
 
         {/* Step 3 */}
-        <div className="mt-[50px] flex w-full flex-col items-start justify-start sm:items-end sm:justify-end">
+        <div className="flex_end mt-[100px] w-full flex-col">
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, ease: "easeIn", type: "tween" }}
-            className="flex max-w-prose flex-row-reverse items-center gap-1 sm:flex-row sm:gap-3"
+            className="flex max-w-prose"
           >
-            <motion.h3 className="work_header">
-              Step 3: Participate in Rotations
+            <motion.h3 className="work_header !bg-gradient-to-r from-blue-300 via-blue-500 to-blue-700 bg-clip-text text-nowrap text-transparent">
+              Step 3: Rotations
             </motion.h3>
-            <div className="flex h-[50px] w-[20px] rounded-sm bg-blue-500">
-              {" "}
-            </div>
           </motion.div>
+
           <p className="work_description">
-            Enjoy secure and automated group contributions.
+            Enjoy seamless contributions and rotations
           </p>
           <div className="motion-preset-expand motion-duration-1000 flex">
             <Icon3dRotate className="h-[100px] w-[100px] text-blue-500 sm:h-[200px] sm:w-[200px]" />
